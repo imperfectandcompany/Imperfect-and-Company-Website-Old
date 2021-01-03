c1336968db5e2c637bcd14956fddabaf33c375d8
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-01-03 12:59:35 -0500

    SEO SETUP

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 <html lang="en">
     <head>
         <title>Imperfect and Company</title>
+		<!-- CSS -->
 		<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+		<!-- Desired link -->
+		<link rel="canonical" href="https://imperfectandcompany.com/">
+		<!-- Favicon -->
 		<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
 		<link rel="manifest" href="site.webmanifest">
 		<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+		<!-- SEO METADATA -->
+		<meta name="description" content="Imperfect and Company. In a world of people trying to be perfect, sometimes you gotta own that you're imperfect but not alone, you got company." />
+		<meta name="keywords" content="imperfectandcompany, imperfect and company, imperfectgamers, postogon, imperfect gamers, imperfectsounds, imperfect sounds, internships" />
 		<meta name="msapplication-TileColor" content="#2d89ef">
+		<meta property="og:title" content="Imperfect and Company - Internships Available" />
+		<meta property="og:description" content="Imperfect but not alone, you got company. Imperfect and Company is accepting internship applications." />
+		<!-- switch to 'https' oncet the new cdn subdomain ssl cert is mitigated -->
+		<meta property="og:image" content="http://cdn.imperfectandcompany.com/assets/22543831959.png" />
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="630" />
 		<meta name="theme-color" content="#ffffff">
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -16,7 +29,14 @@
     </head>
 
 <body>
+<h1>Imperfect and Company</h1>
+<h2>Imperfect in a perfect way</h2
+<h3>Home page</h3>
+<h4>Updates</h4>
+<h5>Projects</h5>
 
+<!-- switch to 'https' oncet the new cdn subdomain ssl cert is mitigated -->
+<img src="http://cdn.imperfectandcompany.com/assets/483193522.png" alt="Imperfect and Company - Logo" />
 
 </body>
 
