beef6d32588b8beb2251c3e5d25ba01538b66e3e
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-01-03 15:54:59 -0500

    Login page

--- login/index.php
+++ login/index.php
@@ -4,8 +4,6 @@
         <title>Imperfect and Company</title>
 		<!-- CSS -->
 		<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-		<!-- JS -->
-		<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
 		<!-- Desired link -->
 		<link rel="canonical" href="https://imperfectandcompany.com/">
 		<!-- Favicon -->
@@ -52,19 +50,10 @@
             <div class="w-full relative">
                 <div class="md:mt-6">
                     <div class="text-center font-semibold text-black">
-                        Register for an account
-                    </div>
-                    <div class="text-center font-base text-black">
-                        This is required to access restricted sections and submit forms
+                        Log In
                     </div>
                     <form class="mt-8" x-data="{password: '',password_confirm: ''}">
                         <div class="mx-auto max-w-lg ">
-                            <div class="py-1">
-                                <span class="px-1 text-sm text-gray-600">Username</span>
-                                <input placeholder="" type="text"
-                                       class="text-md block px-3 py-2 rounded-lg w-full
-                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
-                            </div>
                             <div class="py-1">
                                 <span class="px-1 text-sm text-gray-600">Email</span>
                                 <input placeholder="" type="email"
@@ -77,64 +66,7 @@
                                        class="text-md block px-3 py-2 rounded-lg w-full
                 bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
                             </div>
-                            <div class="py-1">
-                                <span class="px-1 text-sm text-gray-600">Confirm Password</span>
-                                <input placeholder="" type="password" x-model="password_confirm"
-                                       class="text-md block px-3 py-2 rounded-lg w-full
-                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
-                            </div>
-                            <div class="flex justify-start mt-3 ml-4 p-1">
-                                <ul>
-                                    <li class="flex items-center py-1">
-                                        <div :class="{'bg-green-200 text-green-700': password == password_confirm && password.length > 0, 'bg-red-200 text-red-700':password != password_confirm || password.length == 0}"
-                                             class=" rounded-full p-1 fill-current ">
-                                            <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path x-show="password == password_confirm && password.length > 0" stroke-linecap="round"
-                                                      stroke-linejoin="round" stroke-width="2"
-                                                      d="M5 13l4 4L19 7"/>
-                                                <path x-show="password != password_confirm || password.length == 0" stroke-linecap="round"
-                                                      stroke-linejoin="round" stroke-width="2"
-                                                      d="M6 18L18 6M6 6l12 12"/>
 
-                                            </svg>
-                                        </div>
-                                        <span :class="{'text-green-700': password == password_confirm && password.length > 0, 'text-red-700':password != password_confirm || password.length == 0}"
-                                              class="font-medium text-sm ml-3"
-                                              x-text="password == password_confirm && password.length > 0 ? 'Passwords match' : 'Passwords do not match' "></span>
-                                    </li>
-                                    <li class="flex items-center py-1">
-                                        <div :class="{'bg-green-200 text-green-700': password.length > 7, 'bg-red-200 text-red-700':password.length < 7 }"
-                                             class=" rounded-full p-1 fill-current ">
-                                            <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path x-show="password.length > 7" stroke-linecap="round"
-                                                      stroke-linejoin="round" stroke-width="2"
-                                                      d="M5 13l4 4L19 7"/>
-                                                <path x-show="password.length < 7" stroke-linecap="round"
-                                                      stroke-linejoin="round" stroke-width="2"
-                                                      d="M6 18L18 6M6 6l12 12"/>
-
-                                            </svg>
-                                        </div>
-                                        <span :class="{'text-green-700': password.length > 7, 'text-red-700':password.length < 7 }"
-                                              class="font-medium text-sm ml-3"
-                                              x-text="password.length > 7 ? 'The minimum length is reached' : 'At least 8 characters required' "></span>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="flex justify-start">
-                                <label class="block text-gray-500 font-bold my-4 flex items-center">
-                                    <input class="leading-loose text-pink-600 top-0" type="checkbox"/>
-                                    <span class="ml-2 text-sm py-2 text-gray-600 text-left">I accept the
-                                          <a href="#"
-                                             class="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
-                                           Terms and Conditions
-                                          </a>and
-                                          <a href="#"
-                                             class="font-semibold text-black border-b-2 border-gray-200 hover:border-gray-500">
-                                            information data policy.</a>
-                                    </span>
-                                </label>
-                            </div>
                             <button class="mt-3 text-lg font-semibold
             bg-gray-800 w-full text-white rounded-lg
             px-6 py-3 block shadow-xl hover:text-white hover:bg-black">
@@ -146,11 +78,10 @@
 				
 				
                     <div class="text-sm font-semibold block py-6 flex justify-center">
-                        <a href="../login/"
-                           class="text-black font-normal border-b-2 border-gray-200 hover:border-teal-500">Go to 
-                            <span class="text-black font-semibold">
-            Login
-          </span>
+                        <a href="../register/"
+                           class="text-black font-normal">No account? 
+                            <br><span class="text-black  border-b-2 border-gray-200 hover:border-gray-500 font-semibold">
+            Register        </span>
                         </a>
                     </div>
 
